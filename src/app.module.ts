a2afe22b7a8a01a1a8e171c5e5c7f03c794900d5
Igor-Lazarin-Oliani <igorloliani@gmail.com>
2026-06-11 12:06:49 -0400

    Módulos principais adicionados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GamesModule } from './src/modules/games/games.module';
+import { GamesModule } from './games/games.module';
+import { Module } from './games/modules/.module';
+import { GamesModule } from './modules/games/games.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [GamesModule, Module, ReviewsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
